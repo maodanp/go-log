5ede4f43588f56fd3d5cfc7e6f21d00a486be620
Maodanping <673698750@qq.com>
2016-09-04 22:38:43 +0800

    commit log

---
+++ log.go
@@ -0,0 +1,107 @@
+package log
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+const (
+	LOG_DEBUG    = 1
+	LOG_INFO     = 2
+	LOG_WARNINIG = 3
+	LOG_ERROR    = 4
+	LOG_FATAL    = 5
+)
+
+var LOG_LEVEL_MAP = map[int]string{
+	LOG_DEBUG:    "DEBUG",
+	LOG_INFO:     "INFO",
+	LOG_WARNINIG: "WARNING",
+	LOG_FATAL:    "FATAL",
+}
+
+// NewLoggerByPath creates a new logger by fileName
+func NewLoggerByFileName(fileName string) *logger {
+	//return &logger{log.New(w, prefix, log.LstdFlags)}
+	f, err := os.OpenFile(fileName, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	if err != nil {
+		fmt.Printf("NewLoggerByFileName error: %+v", err)
+		return nil
+	}
+	return NewLogger(f, "opentsdb", log.LstdFlags)
+}
+
+// NewLoggerDiscard create a new logger.
+// But do without anything
+func NewLoggerDiscard() *logger {
+	return NewLogger(ioutil.Discard, "", log.Lshortfile|log.LstdFlags)
+}
+
+// NewLogger creates a new logger.
+// it write to io.Writer
+// the prefix appears at the beginning of each generaged log line
+func NewLogger(w io.Writer, prefix string, flag int) *logger {
+	return &logger{
+		log: log.New(w, prefix, flag),
+	}
+}
+
+type logger struct {
+	log   *log.Logger
+	level int
+}
+
+func (l *logger) SetLogLevel(logLevel int) {
+	l.level = logLevel
+}
+
+func (l *logger) Debug(args ...interface{}) {
+	l.output(LOG_DEBUG, args)
+}
+
+func (l *logger) Debugf(f string, args ...interface{}) {
+	l.outputf(LOG_DEBUG, f, args)
+}
+
+func (l *logger) Warning(args ...interface{}) {
+	l.output(LOG_WARNINIG, args)
+}
+
+func (l *logger) Warningf(f string, args ...interface{}) {
+	l.outputf(LOG_WARNINIG, f, args)
+}
+
+func (l *logger) Error(args ...interface{}) {
+	l.output(LOG_ERROR, args)
+}
+
+func (l *logger) Errorf(f string, args ...interface{}) {
+	l.outputf(LOG_ERROR, f, "")
+}
+
+func (l *logger) Fatal(args ...interface{}) {
+	l.output(LOG_FATAL, args)
+}
+
+func (l *logger) Fatalf(f string, args ...interface{}) {
+	l.outputf(LOG_FATAL, f, "")
+}
+
+func (l *logger) output(level int, args ...interface{}) {
+	if l.level|l.level > level {
+		return
+	}
+	s := "[" + LOG_LEVEL_MAP[level] + "] " + fmt.Sprint(args...)
+	l.log.Output(2, s)
+}
+
+func (l *logger) outputf(level int, f string, args ...interface{}) {
+	if l.level|l.level > level {
+		return
+	}
+	s := fmt.Sprintf(f, args...)
+	l.log.Output(2, s)
+}
---
+++ log_test.go
@@ -0,0 +1,49 @@
+package log
+
+import (
+	"log"
+	"os"
+	"testing"
+)
+
+type Foo struct{}
+type Bar struct {
+	one string
+	two int
+}
+
+func TestNewLogger(t *testing.T) {
+	log := NewLogger(os.Stdout, "", log.Lshortfile|log.LstdFlags)
+	log.SetLogLevel(LOG_DEBUG)
+	f := &Foo{}
+	b := &Bar{"asfd", 3}
+	for _, test := range []interface{}{
+		1234,
+		"asdf",
+		f,
+		b,
+	} {
+		log.Debug("hello", test)
+		log.Debugf("something, %v", test)
+		log.Warning(test)
+		log.Warningf("something, %v", test)
+	}
+}
+
+func TestLoggerFileName(t *testing.T) {
+	log := NewLoggerByFileName("./test.log")
+	log.SetLogLevel(LOG_DEBUG)
+	f := &Foo{}
+	b := &Bar{"asfd", 3}
+	for _, test := range []interface{}{
+		1234,
+		"asdf",
+		f,
+		b,
+	} {
+		log.Debug("hello", test)
+		log.Debugf("something, %v", test)
+		log.Warning(test)
+		log.Warningf("something, %v", test)
+	}
+}
